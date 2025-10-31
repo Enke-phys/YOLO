f123ab4078b38b35d0578a903d312266a60f1733
Enke-phys <239130166+Enke-phys@users.noreply.github.com>
2025-10-31 08:50:02 +0100

    aktu

--- commands.sh
+++ commands.sh
@@ -1,22 +1,25 @@
 #Training starten
-yolo detect train \
-  model=yolov9e.pt \
-  data=/Users/enke/Projects/Hirnventile/YOLO/data/dataset.yaml \
-  device=0 \
-  epochs=50 \
-  imgsz=640 \
-  batch=16 \
-  workers=4 \
-  optimizer=AdamW \
-  lr0=0.001 \
-  weight_decay=0.0005 \
+#yolo detect train \
+#  model=yolov9e.pt \
+#  data=/Users/enke/Projects/Hirnventile/YOLO/data/dataset.yaml \
+#  device=0 \
+#  epochs=50 \
+#  imgsz=640 \
+#  batch=16 \
+#  workers=4 \
+#  optimizer=AdamW \
+#  lr0=0.001 \
+#  weight_decay=0.0005 \
 
 #Validierung starten
 #yolo detect val \
 #  data=/Users/enke/Projects/Hirnventile/YOLO/data/dataset.yaml \
-#  model=runs/detect/train/weights/best.pt
+#  model=runs/detect/train3/weights/best.pt \
+#  device=cpu
 
 #Inferenz starten
-#yolo predict \
-#  model=runs/detect/train/weights/best.pt \
-#  source=/Pfad/zu/Testbildern
+yolo predict \
+  model=/Users/enke/Projects/Hirnventile/YOLO/runs/detect/bestv2.pt \
+  source=/Users/enke/Projects/Hirnventile/YOLO/VP_Shunts_Externe_Validierung \
+  max_det=1 \
+  iou=0.45
